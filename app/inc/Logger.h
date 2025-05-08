fab8faa006308ae32e1b6b16e65e40e26665d858
WoobieApps <piotrasel@gmail.com>
2025-05-08 22:16:49 +0200

    Merge feature/logger to main
    - Add log formatting capabilities

--- app/inc/Logger.h
+++ app/inc/Logger.h
@@ -4,11 +4,11 @@
 
 std::string getTimestampNow();
 
-#define LOG(level,msg) \
-        std::printf("%s : [%s] %s:%d: %s\n", \
-        getTimestampNow().c_str(), level,__FUNCTION__, __LINE__, msg)
+#define LOG(level, fmt, ...) \
+        std::printf("%s [%s] %s:%d: " fmt "\n", \
+        getTimestampNow().c_str(), level, __FUNCTION__, __LINE__, ##__VA_ARGS__);
 
-#define LOG_INF(msg) LOG("INF", msg)
-#define LOG_DBG(msg) LOG("DBG", msg)
-#define LOG_WRN(msg) LOG("WRN", msg)
-#define LOG_ERR(msg) LOG("ERR", msg)
+#define LOG_INF(fmt, ...) LOG("INF", fmt, ##__VA_ARGS__)
+#define LOG_DBG(fmt, ...) LOG("DBG", fmt, ##__VA_ARGS__)
+#define LOG_WRN(fmt, ...) LOG("WRN", fmt, ##__VA_ARGS__)
+#define LOG_ERR(fmt, ...) LOG("ERR", fmt, ##__VA_ARGS__)
