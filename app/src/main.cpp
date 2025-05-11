abb0f5ad0bda4ef8f110587af3716f7e2e945ea7
WoobieApps <piotrasel@gmail.com>
2025-05-11 20:56:31 +0200

    Working integration of WxWidgets
    Integrated WxWidgets as a flatpak
    module for GUI part of the project.

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,10 +1,32 @@
+#include <wx/wx.h>
+#include "InputMgr.h"
 #include "Logger.h"
 
-int main(int argc, char *argv[])
+class MyApp : public wxApp
 {
-    LOG_INF("Starting application with %d arguments", argc);
-    LOG_DBG("Debugging information: %s", argv[0]);
-    LOG_WRN("Warning: This is a warning message");
-    LOG_ERR("Error: This is an error message");
-    return 0;  
+public:
+    virtual bool OnInit() override;
+};
+
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame()
+        : wxFrame(nullptr, wxID_ANY, "DeckRC", wxDefaultPosition, wxSize(400, 300),
+                  wxDEFAULT_FRAME_STYLE) // Standard window with close/min/max buttons
+    {
+        // Add "Hello, World!" label
+        new wxStaticText(this, wxID_ANY, "Hello, World!", wxPoint(20, 20));
+    }
+};
+
+wxIMPLEMENT_APP(MyApp);
+
+bool MyApp::OnInit()
+{
+    Input::InputMgr::InitInstance();
+    LOG_INF("DeckRC started");
+    MyFrame* frame = new MyFrame();
+    frame->Show(true);
+    return true;
 }
